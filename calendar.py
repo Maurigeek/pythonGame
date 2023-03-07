be3cef9e4bc691081bd359825490aba9bbba0c1d
maurigeek <mmedeho@gmail.com>
2023-03-07 09:32:01 +0100

    Création du project et configuration des constantes

---
+++ calendar.py
@@ -0,0 +1,12 @@
+"""Créez des calendriers mensuels, 
+enregistrés dans un fichier texte et adaptés à l'impression."""
+
+import datetime
+
+# Définir les constances
+
+DAYS = ('Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi',
+        'Vendredi', 'Samedi')
+MONTHS = ('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 
+          'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 
+          'Décembre')
