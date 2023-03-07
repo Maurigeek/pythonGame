7d22b851e84285e08643cdfccf2ad7fce3c61c68
maurigeek <mmedeho@gmail.com>
2023-03-07 11:30:29 +0100

    Function 'getCalendarFor' qui construit le calendrier

--- calendar.py
+++ calendar.py
@@ -22,7 +22,7 @@ while True:
         break
 
     print('Veuillez entrer une année numérique, comme 2023')
-   Please enter a number from 1 to 12.
+    continue
 
 while True:
     print('Entrer le mois pour le calendrier, 1-12')
@@ -39,3 +39,51 @@ while True:
     print('Veuillez entrer un nombre de 1 à 12')
 
 
+def getCalendarFor(year, month):
+    calText = '' # CalText contiendra la chaine de notre calendrier
+
+    # Mettre lle mois et l'année en haut du calendrier
+    calText += (' ' * 33) + MONTHS[month - 1] + ' ' + str(year) + '\n'
+
+    # Ajouter les libellés des jours de la seaine au calendrier
+    calText += '...Dimanche....Lundi....Mardi....Mercredi....Jeudi....Vendredi....Samedi...\n'
+
+    # La chaîne de lignes horizontales qui séparent les semaines
+    weekSeparator = ('+----------' * 7) + '\n'
+
+    # Les lignes vides ont dix espaces entre le séparateur de jours
+    blankRow = ('|          ' * 7) + '|\n'
+
+    # Obtenir la première date du mois (Le module datetime gère tous)
+    currentDate = datetime.date(year, month, 1)
+    # Aller en arrière jusqu'à ce que ça soit le dimanche (weekday retourne 6)
+    # pour un dimanche.
+    while currentDate.weekday() != 6:
+        currentDate -= datetime.timedelta(days=1)
+    
+    while True: # Bloucle  sur chaque semaine du mois
+        calText += weekSeparator
+
+        # dayNumberRow est la ligne avec les numéros du jour de la semaine
+        dayNumberRow = ''
+        for i in range(7):
+            dayNumberLabel = str(currentDate.day).rjust(2)
+            dayNumberRow += '|' + dayNumberLabel + (' ' * 8)
+            currentDate += datetime.timedelta(days=1) # Aller au jour suivant
+        dayNumberRow += '|\n' # Ajouter une ligne verticale après Samedi
+
+        # Ajouter la ligne du numéro du jour et 3 lignes vide au texte du calendrier
+        calText += dayNumberRow 
+        for i in range(4):
+            calText += blankRow
+        
+        # Vérifier si nous avon fini avec le mois
+        if currentDate.month != month:
+            break
+
+    # Alouter des lignes horizontales tout en bas du calendrier
+    calText += weekSeparator
+    return calText
+
+calText = getCalendarFor(year, month)
+print (calText)
